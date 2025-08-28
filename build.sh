c461890e75c553cf91baed605043bb66379cbd3b
Jarosław Dutka <jaroslawjdutka@gmail.com>
2025-08-28 14:58:37 +0200

    WIP

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cp README.md addon/DOCS.md
-cp README.md addon/.
-
-VERSION=$(cat addon/config.yaml| grep version | grep -P -o "[\d\.]*")
-
-echo git tag -a $VERSION -m $VERSION
